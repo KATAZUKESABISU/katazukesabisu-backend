1a7781f9b96b3c8a269287c5f478751aba5697aa
TruongVanLam <truongvanlam176@gmail.com>
2023-09-19 23:22:57 +0700

    fixbug upload img to cloudinary

--- seed/index.js
+++ seed/index.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const mongoose = require("mongoose");
 
 const MstInquiryModel = require("../models/mst_inquiry_model");
